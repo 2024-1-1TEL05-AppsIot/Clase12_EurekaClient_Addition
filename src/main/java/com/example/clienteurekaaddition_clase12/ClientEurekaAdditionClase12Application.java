48fbb44fd60f13191cade523deeb3670c272c58e
oscar-diaz <oscarpucp@gmail.com>
2024-06-10 17:53:11 -0500

    Clase 12 - Introducción a los microservicios - EurekaClient Addition

---
+++ src/main/java/com/example/clienteurekaaddition_clase12/ClientEurekaAdditionClase12Application.java
@@ -0,0 +1,14 @@
+package com.example.clienteurekaaddition_clase12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ClientEurekaAdditionClase12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientEurekaAdditionClase12Application.class, args);
+    }
+
+}
